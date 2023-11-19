48785e61035ea33cf839495c427d1dc6de50c3c2
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-11-19 12:14:51 -0300

    Adicionado o componente Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div style={{background: "#E5E5E5", width: "100%", minHeight: "34px", display: "flex", alignItems: "center", justifyContent: "center"}}>
+        <p style={{fontWeight: 400, fontSize: "12px", lineHeight: "15px"}}>MKS Sistemas © Todos os direitos reservados</p>
+    </div>
+  )
+}
