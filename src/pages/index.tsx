dd5822b287ea06c36b91e720d1810f7bf6ea1788
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-11-21 12:02:26 -0300

    Atualizado o arquivo Home da pagina de pages com o Provider/store em toda a aplicação e dentro do Provider adiciona o arquivo ProductList

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
+import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
+import ProductList from '@/components/ProductList/ProductList'
+import store from '@/redux/store'
+import { Provider } from 'react-redux';
 
 export default function Home() {
   return (
-    <main>
-      <Header />
-      <Footer />
-    </main>
+    <Provider store={store}>
+      <main>
+        <Header />
+        <ProductList />
+        <Footer />
+      </main>
+    </Provider>
   )
 }
